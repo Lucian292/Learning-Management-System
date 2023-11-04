ea598e5fc61bb1bff59b0697251f1d74bf7e20db
Lucian <lucian_munteanu@mail.ru>
2023-11-04 20:17:41 +0200

    First attempt of class organisation

---
+++ Learning-Management-System/LearningManagementSystem.Application/Features/Categories/Commands/CreateQuestion/CreateQuestionCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Application.Features.Categories.Commands.CreateQuestion
+{
+    public class CreateQuestionCommandResponse
+    {
+    }
+}
---
+++ Learning-Management-System/NewTests/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace NewTests
+{
+    public class Program
+    {
+
+    }
+}
