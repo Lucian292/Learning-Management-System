266c4e43ccef20fdf07930d27e0b9c1c258d22d8
Lucian <lucian_munteanu@mail.ru>
2024-01-10 15:07:00 +0200

    adaugat validari la update chapter

--- Learning-Management-System/LearningManagementSystem.Application/Features/Chapters/Commands/UpdateChapter/UpdateChapterCommandValidator.cs
+++ Learning-Management-System/LearningManagementSystem.Application/Features/Chapters/Commands/UpdateChapter/UpdateChapterCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
 using System.Text;
 
 namespace LearningManagementSystem.Application.Features.Chapters.Commands.UpdateChapter
@@ -11,6 +12,29 @@ namespace LearningManagementSystem.Application.Features.Chapters.Commands.Update
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull()
                 .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.Chapter.Content)
+               .Must(content => BeAValidPdfFile(content))
+               .WithMessage("Invalid PDF file. File must be a PDF and should not exceed 15 MB.");
+        }
+
+        private bool BeAValidPdfFile(byte[] pdfFile)
+        {
+            // Verifică dacă fișierul este PDF și are o dimensiune mai mică sau egală cu 15 MB
+            if (pdfFile == null || pdfFile.Length == 0)
+            {
+                // Fisierul nu este obligatoriu, deci este valid dacă nu există sau are dimensiunea zero
+                return true;
+            }
+
+            // Verifică dacă este un fișier PDF cu dimensiunea mai mică sau egală cu 15 MB
+            return IsPdfFile(pdfFile) && pdfFile.Length <= 15 * 1024 * 1024; // 15 MB
+        }
+
+        private bool IsPdfFile(byte[] file)
+        {
+            const string pdfSignature = "%PDF";
+            return file.Length >= pdfSignature.Length &&
+                   Encoding.ASCII.GetString(file, 0, pdfSignature.Length) == pdfSignature;
         }
     }
 }
